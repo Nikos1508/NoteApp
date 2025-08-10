d7d095afd86c82c93f0c894cab9d5b46bcee285e
Nikos Tsilas <tsilasnikos@gmail.com>
2025-08-10 06:49:49 -0400

    Make the app not to crash (on the  first steps at least)

--- app/src/main/java/com/example/noteapp/SupabaseViewModel.kt
+++ app/src/main/java/com/example/noteapp/SupabaseViewModel.kt
@@ -11,8 +11,8 @@ import io.ktor.http.*
 import kotlinx.serialization.json.Json
 
 class SupabaseViewModel {
-    private val baseUrl = "https://lrqclmhdgdyvcechgxrq.supabase.co"
-    private val apiKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImxycWNsbWhkZ2R5dmNlY2hneHJxIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDI3MTYxNTYsImV4cCI6MjA1ODI5MjE1Nn0.o9tMHAvC9s2WhzB4gT1-LTUYhuBamStRh4aee9DaHTI"
+    private val baseUrl = "https://gcxpaqzaqojybawzzuro.supabase.co"
+    private val apiKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImdjeHBhcXphcW9qeWJhd3p6dXJvIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTQ3OTEwMzEsImV4cCI6MjA3MDM2NzAzMX0.pt_JeDbvJ8OzgYDe1Vhj8nwVInhNY96I67Qft5Vyr2E"
 
     private val client = HttpClient(CIO) {
         install(ContentNegotiation) {
@@ -21,14 +21,14 @@ class SupabaseViewModel {
     }
 
     suspend fun getNotes(): List<Note> {
-        return client.get("$baseUrl/notes") {
+        return client.get("$baseUrl/rest/v1/notes") {
             header("apikey", apiKey)
             header("Authorization", "Bearer $apiKey")
         }.body()
     }
 
     suspend fun getNoteById(id: String): Note {
-        return client.get("$baseUrl/notes") {
+        return client.get("$baseUrl/rest/v1/notes") {
             header("apikey", apiKey)
             header("Authorization", "Bearer $apiKey")
             parameter("id", "eq.$id")
@@ -36,7 +36,7 @@ class SupabaseViewModel {
     }
 
     suspend fun createNote(title: String, htmlContent: String): Note {
-        return client.post("$baseUrl/notes") {
+        return client.post("$baseUrl/rest/v1/notes") {
             header("apikey", apiKey)
             header("Authorization", "Bearer $apiKey")
             contentType(ContentType.Application.Json)
@@ -45,7 +45,7 @@ class SupabaseViewModel {
     }
 
     suspend fun updateNote(id: String, title: String, htmlContent: String) {
-        client.patch("$baseUrl/notes?id=eq.$id") {
+        client.patch("$baseUrl/rest/v1/notes?id=eq.$id") {
             header("apikey", apiKey)
             header("Authorization", "Bearer $apiKey")
             contentType(ContentType.Application.Json)
